1c30e2de3fe11cae95e4472a4f2df170d65f5ed3
viktor <viktor.com9988@gmail.com>
2018-06-13 13:01:04 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils
-sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
+sudo apt-get --force-yes -y install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils
+sudo apt-get --force-yes -y install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
 sudo add-apt-repository ppa:bitcoin/bitcoin
 sudo apt-get update
-sudo apt-get install libdb4.8-dev libdb4.8++-dev
-sudo apt-get install libminiupnpc-dev
-sudo apt-get install libzmq3-dev
-sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
-sudo apt-get install libqrencode-dev
-sudo apt-get install unzip
+sudo apt-get --force-yes -y install libdb4.8-dev libdb4.8++-dev
+sudo apt-get --force-yes -y install libminiupnpc-dev
+sudo apt-get --force-yes -y install libzmq3-dev
+sudo apt-get --force-yes -y install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
+sudo apt-get --force-yes -y install libqrencode-dev
+sudo apt-get --force-yes -y install unzip
+if ! [ -d /root/sh/1lin ]; then
 git clone https://github.com/vdovychenko99/1lin
 git clone https://github.com/vdovychenko99/cli
+fi
 cd 1lin
-unzip charityd.zip
+unzip charityd
 chmod 777 charityd
 cd ..
 cd cli
@@ -24,16 +26,15 @@ cd 1lin
 ./charityd
 IP=''
 PRIVKEY=''
-echo -n -e "Enter ip adderss:\n"
+echo -n -e "Enter IP adderss:\n"
 read IP
-echo -n -e "Enter PRIVKE:\n"
+echo -n -e "Enter PRIVATE_KEY:\n"
 read PRIVKEY
  cat << EOF > /root/.charity/charity.conf
 rpcuser=charity123
 rpcpassword=rpcpassword123
 rpcallowip=127.0.0.1
-rpcport=3301
-port=3300
+rpcport=3300
 listen=1
 server=1
 daemon=1
