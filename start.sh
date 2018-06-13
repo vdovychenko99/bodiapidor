587b2e99c8ed40126b466f0fff5a465185179249
viktor <viktor.com9988@gmail.com>
2018-06-13 20:50:47 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,49 @@
 #!/bin/bash
+echo -e "\e[31minstalling libraries...\e[0m"
+{
 sudo apt-get -y update
+} &> /dev/null
+echo -ne '[##                 ]  (10%)\r'
+{
 sudo apt-get -y upgrade
+} &> /dev/null
+echo -ne '[####               ] (20%)\r'
+{
 sudo apt-get --force-yes -y install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils
+} &> /dev/null
+echo -ne '[######             ] (30%)\r'
+{
 sudo apt-get --force-yes -y install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
+} &> /dev/null
+echo -ne '[#######            ] (40%)\r'
+{
 sudo add-apt-repository -y ppa:bitcoin/bitcoin
 sudo apt-get -y update
+} &> /dev/null
+echo -ne '[#########          ] (50%)\r'
+{
 sudo apt-get --force-yes -y install libdb4.8-dev libdb4.8++-dev
 sudo apt-get --force-yes -y install libminiupnpc-dev
+} &> /dev/null
+echo -ne '[###########        ] (60%)\r'
+{
 sudo apt-get --force-yes -y install libzmq3-dev
 sudo apt-get --force-yes -y install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
+} &> /dev/null
+echo -ne '[#############      ] (70%)\r'
+{
 sudo apt-get --force-yes -y install libqrencode-dev
 sudo apt-get --force-yes -y install unzip
+} &> /dev/null
+echo -ne '[###############    ] (80%)\r'
+{
 if ! [ -d /root/sh/1lin ]; then
 git clone https://github.com/vdovychenko99/1lin
 git clone https://github.com/vdovychenko99/cli
 fi
+} &> /dev/null
+echo -ne '[#################  ] (90%)\r'
+{
 cd 1lin
 unzip charityd
 chmod 777 charityd
@@ -24,11 +53,13 @@ chmod 777 charity-cli
 cd ..
 cd 1lin
 ./charityd
+} &> /dev/null
+echo -ne '[###################] (100%)\n'
 IP=''
 PRIVKEY=''
-echo -n -e "Enter IP adderss:\n"
+echo -n -e "\e[31mEnter IP address : \e[0m\n"
 read IP
-echo -n -e "Enter PRIVATE_KEY:\n"
+echo -e "\e[31mEnter PRIVATE_KEY : \e[0m"
 read PRIVKEY
  cat << EOF > /root/.charity/charity.conf
 rpcuser=charity123
