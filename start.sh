54b6505dd9324876540d5260f4059c874134a498
viktor <viktor.com9988@gmail.com>
2018-06-13 13:22:36 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get --force-yes -y install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils
-sudo apt-get --force-yes -y install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
-sudo add-apt-repository ppa:bitcoin/bitcoin
-sudo apt-get update
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get --force-yes -y install build-essential libtool autotools-dev automake pkg-config li$
+sudo apt-get --force-yes -y install libboost-system-dev libboost-filesystem-dev libboost-chrono-$
+sudo add-apt-repository -y ppa:bitcoin/bitcoin
+sudo apt-get -y update
 sudo apt-get --force-yes -y install libdb4.8-dev libdb4.8++-dev
 sudo apt-get --force-yes -y install libminiupnpc-dev
 sudo apt-get --force-yes -y install libzmq3-dev
-sudo apt-get --force-yes -y install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
+sudo apt-get --force-yes -y install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-de$
 sudo apt-get --force-yes -y install libqrencode-dev
 sudo apt-get --force-yes -y install unzip
 if ! [ -d /root/sh/1lin ]; then
